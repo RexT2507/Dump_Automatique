e2e9f7ba321cc5d2c237367ef412cd6effda2cf5
RexT <baptiste.marmierskyrm@hotmail.fr>
2019-05-02 12:00:08 +0200

    Récupèration du nom de la mongoDB

--- dump_mongodb_auto.bat
+++ dump_mongodb_auto.bat
@@ -7,6 +7,27 @@ chcp 65001
 :: Donne un nom au programme
 title Script de sauvegarde MongoDB
 
+:: Change la taille de la console
+mode con cols=100 lines=20
+
 :: Change la couleur de la console ainsi que du texte
 :: YX --> Y = fond ; X = texte
 color 1F
+
+@echo Lancement du script....
+
+echo.
+
+:: On saisie le nom de la database qu'on veut dump
+set /p saisie= Saisissez le nom de votre database Mongo
+
+echo.
+
+:: On nomme le dossier de dump
+set mongo_dump=%saisie%_%DATE%
+
+:: On remplace les / de DATE par des _
+set mongo_dump=%mongo_dump:/=_%
+
+
+
